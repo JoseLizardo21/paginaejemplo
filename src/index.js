d55fbd584269bae52ab1d0f8ccd3dd48e65bce44
Jose <jose.lizardo21.cont@gmail.com>
2022-10-12 18:42:04 -0500

    nuevo2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 //initializations
 const app = express();
 //settings
-app.set('port', 3000 || process.env.PORT);
+app.set('port', process.env.PORT || 3000);
 
 //Routes
 
@@ -11,5 +11,5 @@ app.set('port', 3000 || process.env.PORT);
 app.use(express.static(path.join(__dirname, 'public')));
 //Starting the server
 app.listen(app.get('port'), ()=>{
-    console.log(`Server on port ${app.get('port')}`);
+    console.log(`Servidor funcionando en el puerto ${app.get('port')}`);
 });
